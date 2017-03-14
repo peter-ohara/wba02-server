33ec9d8598af85915d5c28ca5d4e309cd0914aba
Peter O'Hara Adu <ohara.invent@gmail.com>
2017-03-14 14:18:05 +0000

    Add comments to quiz_contents

--- app/controllers/android_v1/take_quiz_screen_controller.rb
+++ app/controllers/android_v1/take_quiz_screen_controller.rb
@@ -37,11 +37,53 @@ class AndroidV1::TakeQuizScreenController < ApplicationController
       content[:title] = quiz_content.is_a?(Header) ? quiz_content.title : quiz_content.number
       content[:content] = quiz_content.is_a?(Header) ? quiz_content.content : quiz_content.question
       content[:choices] = quiz_content.choices if quiz_content.is_a?(MultipleChoiceQuestion)
-      content[:comments] = quiz_content.comments
+      content[:comments] = get_formatted_comments(quiz_content.comments)
       content[:answers] = []
       quiz_contents.push(content)
     end
     quiz_contents
+  end
+
+  def get_formatted_comments(raw_comments)
+    # id                      // Required
+    # parent                  // Required if replying is enabled
+    # created                 // Required
+    # modified                // Required if editing is enabled
+    # content                 // Either content or fileURL must be present
+    # fileURL                 // Either content or fileURL must be present
+    # file                    // Required when uploading an attachment
+    # fileMimeType            // Optional
+    # pings                   // Required if pinging is enabled
+    # creator                 // Required if pinging is enabled
+    # fullname                // Required
+    # profileURL              // Optional
+    # profilePictureURL       // Optional
+    # createdByAdmin          // Optional
+    # createdByCurrentUser    // Required if editing is enabled
+    # upvoteCount             // Required if upvoting is enabled
+    # userHasUpvoted          // Required if upvoting is enabled
+
+    formatted_comments = []
+    raw_comments.each do |raw_comment|
+      formatted_comment = {}
+      formatted_comment[:id] = raw_comment.id
+      formatted_comment[:parent] = raw_comment.parent_id
+      formatted_comment[:created] = raw_comment.created_at
+      formatted_comment[:modified] = raw_comment.updated_at
+      formatted_comment[:content] = raw_comment.content
+      # formatted_comment[:fileUrl] = raw_comment.file.url if it is a file
+      # formatted_comment[:file] = raw_comment.file if it is a file
+      # formatted_comment[:fileMimeType] = raw_comment.file if it is a file
+      formatted_comment[:fullname] = raw_comment.user.display_name
+      # formatted_comment[:profileURL] = raw_comment.user.url
+      # formatted_comment[:profilePictureURL] = raw_comment.user.profile_picture_url
+      # formatted_comment[:createdByAdmin] = raw_comment.content
+      formatted_comment[:createdByCurrentUser] = true # correct this
+      formatted_comment[:upvoteCount] = raw_comment.upvotes.count
+      formatted_comment[:userHasUpvoted] = false # correct this
+      formatted_comments.push(formatted_comment)
+    end
+    formatted_comments
   end
 
   def get_quiz_content_type(quiz_content)
