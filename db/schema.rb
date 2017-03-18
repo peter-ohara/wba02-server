1de6cd245134c4ab95cf1302894989ffa4ea7c63
Peter O'Hara Adu <ohara.invent@gmail.com>
2017-03-18 11:01:45 +0000

    Add profile_url column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170321095210) do
+ActiveRecord::Schema.define(version: 20170321095211) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -168,6 +168,7 @@ ActiveRecord::Schema.define(version: 20170321095210) do
     t.string   "email"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.string   "profile_url"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
   end
 
