1aa0692e186a6b5b7a35faf2ce7b7fe4d77d7fb1
Peter O'Hara Adu <ohara.invent@gmail.com>
2017-03-20 01:40:58 +0000

    Add answer_histogram to quiz_contents

--- app/serializers/android_v1/quiz_content_serializer.rb
+++ app/serializers/android_v1/quiz_content_serializer.rb
@@ -9,6 +9,7 @@ class AndroidV1::QuizContentSerializer < ActiveModel::Serializer
   attribute :choices, if: 'object.is_a?(MultipleChoiceQuestion)'
   attribute :answer, unless: 'object.is_a?(Header)'
   attribute :explanation, unless: 'object.is_a?(Header)'
+  attribute :answer_histogram
 
   def type
     if object.is_a?(Header)
@@ -44,6 +45,19 @@ class AndroidV1::QuizContentSerializer < ActiveModel::Serializer
       AndroidV1::CommentSerializer.new(comment,
                                        scope: current_user,
                                        scope_name: :current_user)
+    end
+  end
+
+  def answer_histogram
+    defaults = {
+        a: 0,
+        b: 0,
+        c: 0,
+        d: 0
+    }
+    if object.is_a?(MultipleChoiceQuestion)
+      options = Hash[*object.answers.group_by{ |v| v.content }.flat_map{ |k, v| [k, v.size] }]
+      defaults.merge(options)
     end
   end
 
